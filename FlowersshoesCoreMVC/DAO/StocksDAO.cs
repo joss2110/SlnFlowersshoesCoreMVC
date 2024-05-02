2ded779c7ae7822177c8bd5bd97ee582dfb80dfc
MAJO-05 <mariajosegaspar95@gmail.com>
2024-05-02 09:09:34 -0500

    Avance  Majo

---
+++ FlowersshoesCoreMVC/DAO/StocksDAO.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlClient;
+using System.Drawing;
+using FlowersshoesCoreMVC.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FlowersshoesCoreMVC.DAO
+{
+    public class StocksDAO
+    {
+
+        private string cad_sql = "";
+
+        public StocksDAO(IConfiguration cfg)
+        {
+            cad_sql = cfg.GetConnectionString("cn2");
+        }
+
+        public List<TbStock> ListarStocks()
+        {
+            var lista = new List<TbStock>();
+
+            SqlDataReader dr =
+              SqlHelper.ExecuteReader(cad_sql, "PA_LISTAR_STOCKS");
+
+            while (dr.Read())
+            {
+                lista.Add(
+                  new TbStock()
+                  {
+                      Idstock = dr.GetInt32(0),
+                      //Nompro = dr.GetString(1),
+                      //Imagen = dr.GetString(2),
+                      //Color = dr.GetString(3),
+                      //Talla = dr.GetString(4),
+                      Cantidad = dr.GetInt32(0),
+                  });
+            }
+            dr.Close();
+
+            return lista;
+        }
+    }
+}
---
+++ FlowersshoesCoreMVC/DAO/TallaDAO.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using FlowersshoesCoreMVC.Models;
+
+namespace FlowersshoesCoreMVC.DAO
+{
+    public class TallaDAO
+    {
+        private string cad_sql = "";
+
+        public TallaDAO(IConfiguration cfg)
+        {
+            cad_sql = cfg.GetConnectionString("cn2");
+        }
+
+        public List<TbTalla> ListarTallas()
+        {
+            var lista = new List<TbTalla>();
+
+            SqlDataReader dr =
+              SqlHelper.ExecuteReader(cad_sql, "PA_LISTAR_TALLAS");
+
+            while (dr.Read())
+            {
+                lista.Add(
+                  new TbTalla()
+                  {
+                      Idtalla = dr.GetInt32(0),
+                      Talla = dr.GetString(1)
+                  });
+            }
+            dr.Close();
+
+            return lista;
+        }
+    }
+}
