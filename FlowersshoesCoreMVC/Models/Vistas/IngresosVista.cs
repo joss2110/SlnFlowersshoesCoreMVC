56d8cfaa92bb8e0e8cb784c1e4f874b148b55292
sadot-jayo <jsadot30@gmail.com>
2024-05-04 16:45:15 -0500

    Crear Ingreso Vista

---
+++ FlowersshoesCoreMVC/Models/Vistas/IngresosVista.cs
@@ -0,0 +1,8 @@
+﻿namespace FlowersshoesCoreMVC.Models.Vistas
+{
+    public class IngresosVista
+    {
+        public TbIngreso NuevoIngreso { get; set; } = new TbIngreso();
+        public IEnumerable<TbIngreso> listaIngresos { get; set; } = Enumerable.Empty<TbIngreso>();
+    }
+}
