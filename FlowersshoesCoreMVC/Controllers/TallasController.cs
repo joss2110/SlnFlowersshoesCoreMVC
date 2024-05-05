c2a5414f39b6a435d8f7603b0e321cbff7f2642a
MAJO-05 <mariajosegaspar95@gmail.com>
2024-05-04 22:08:45 -0500

    Majo

---
+++ FlowersshoesCoreMVC/Controllers/TallasController.cs
@@ -0,0 +1,34 @@
+﻿using FlowersshoesCoreMVC.Models;
+using FlowersshoesCoreMVC.Models.Vistas;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FlowersshoesCoreMVC.Controllers
+{
+    public class TallasController : Controller
+    {
+        List<TbTalla> lista = new List<TbTalla>();
+        // GET: TallasController
+        public async Task<List<TbTalla>> GetTallas()
+        {
+            using (var httpcliente = new HttpClient())
+            {
+                var respuesta = await httpcliente.GetAsync("http://localhost:5050/api/Tallas/GetTallas");
+                string respuestaAPI = await respuesta.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<TbTalla>>(respuestaAPI)!;
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Tallas(int id, string accion)
+        {
+            lista = await GetTallas();
+            
+
+
+
+            return View(lista);
+        }
+    }
+}
