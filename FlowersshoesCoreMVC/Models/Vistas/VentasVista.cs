a8abd71f303a327f91923b92058daf5f95da95c3
joss2110 <g.j.torres.perez@gmail.com>
2024-05-07 00:45:53 -0500

    ACTUALIZACIONES VENTAS

---
+++ FlowersshoesCoreMVC/Models/Vistas/VentasVista.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowersshoesCoreMVC.Models.Vistas
+{
+    public class VentasVista
+    {
+        public TbCliente nuevoCliente { get; set; } = new TbCliente();
+        public IEnumerable<TbDetalleVenta> listaDetaVenta { get; set; } = Enumerable.Empty <TbDetalleVenta>();
+
+    }
+}
