09930b253bdf50a788fb4c827b13ac5668b61d2c
joss2110 <g.j.torres.perez@gmail.com>
2024-05-08 23:25:00 -0500

    cambios venta

---
+++ FlowersshoesCoreMVC/Models/PA_LISTAR_DETALLE_VENTAS.cs
@@ -0,0 +1,14 @@
+﻿namespace FlowersshoesCoreMVC.Models
+{
+    public class PA_LISTAR_DETALLE_VENTAS
+    {
+        public string imagen { get; set; } = string.Empty;
+        public int idpro { get; set; }
+        public string nompro { get; set; } = string.Empty;
+        public string color { get; set; } = string.Empty;
+        public string talla { get; set; } = string.Empty;
+        public int cantidad { get; set; }
+        public decimal Preciouni { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
